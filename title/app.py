18efdfde68dda99b8cd72cd9e2fe713884d8d3c6
ikmk0515 <73947564+ikmk0515@users.noreply.github.com>
2024-08-18 18:51:32 +0900

    Update app.py

--- title/app.py
+++ title/app.py
@@ -3,4 +3,4 @@ import time
 import numpy as np
 
 st.title("손금 테스트")
-start = st.link_button("시작하기",url="https://apptset-p7dq68x5yxswyvecgv6t9x.streamlit.app/", type="primary")
+start = st.link_button("시작하기",url="https://apptset-jrnp43csihzougzjh7gfyj.streamlit.app/", type="primary")
